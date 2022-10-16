aed39523ff448db17f9e6caeb252eaeff05ee4d1
kelvinmburu <mburuhkelvin@gmail.com>
2022-10-16 21:14:32 +0300

    build: implemented asc & desc features

---
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-header',
+  templateUrl: "./products-header.component.html",
+})
+export class ProductsHeaderComponent implements OnInit {
+sort = 'desc'
+itemsShowCount = '12';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSortUpdated(newSort: string): void{
+    this.sort =  newSort;
+  }
+
+}
