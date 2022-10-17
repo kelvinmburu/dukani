d02996a900f0619967ab4b369afc3f143d25ae25
kelvinmburu <mburuhkelvin@gmail.com>
2022-10-17 23:10:27 +0300

    build: added layout for icons

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-products-header',
   templateUrl: "./products-header.component.html",
 })
 export class ProductsHeaderComponent implements OnInit {
-sort = 'desc'
+
+@Output() columnsCountChange = new EventEmitter<number>();
+sort = 'desc';
 itemsShowCount = 12;
+
   constructor() { }
 
   ngOnInit(): void {
@@ -20,4 +23,7 @@ itemsShowCount = 12;
     this.itemsShowCount = count;
   }
 
+  onColumnsUpdated(colsNum: number): void {
+    this.columnsCountChange.emit(colsNum);
+  }
 }
