1942cf1ca1346618530e2c903bf8be702065d2ab
kelvinmburu <mburuhkelvin@gmail.com>
2022-10-17 23:10:09 +0300

    feat: implemented components layout order

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,14 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: [
-  ]
 })
 export class HomeComponent implements OnInit {
+  cols = 3;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onColumnsCountChange(colsNum: number): void {
+    this.cols = colsNum;
   }
-
 }
